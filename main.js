f36b59f516c416ebb10e221518a7828dc02ef046
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2024-12-11 16:09:12 +0100

    feat: exercise solved

---
+++ main.js
@@ -0,0 +1,13 @@
+// creare un ciclo for che esegue 100 cicli
+for (let i = 0; i <= 100; i++) {
+    // all'interno del for, creare una condizione if che calcola il resto %
+    if((i % 3 === 0) && (i % 5 === 0)){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+    } else if(i % 5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
